c18994722ceeb7f847222f45bf67589dd8459f46
tandersonWakeT <tsanderson2@my.waketech.edu>
2022-02-07 11:00:56 -0500

    initial commit

---
+++ src/main/java/com/hcl/EmpMngmtCLI.java
@@ -0,0 +1,13 @@
+package com.hcl;
+
+/**
+ * Hello world!
+ *
+ */
+public class EmpMngmtCLI 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
